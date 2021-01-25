b36bcf56185a38c1e1b3e12a6708f11f8149e108
monc949 <ciaranmaye8@gmail.com>
2021-01-25 15:02:37 +0000

    .

--- src/View/Table.java
+++ src/View/Table.java
@@ -5,13 +5,13 @@ import javax.swing.JFrame;
 
 
 public class Table {
-    public Table() {
-        JFrame frame = new ProductView();
-        frame.setTitle("Product Table");
-        frame.setSize(900, 300);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        public Table() {
+            JFrame frame = new ProductView();
+            frame.setTitle("Product Table");
+            frame.setSize(900, 300);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     
