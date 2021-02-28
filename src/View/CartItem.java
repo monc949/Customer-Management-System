ac93a7fc0e5da33394b5649a75e9e0e33819a461
monc949 <ciaranmaye8@gmail.com>
2021-02-28 13:30:53 +0000

    Still not working

---
+++ src/View/CartItem.java
@@ -0,0 +1,24 @@
+package View;
+
+import java.awt.Dimension;
+
+import javax.swing.*;
+
+public class CartItem {
+
+    private JPanel container = new JPanel();
+    private JLabel itemName = new JLabel("Temp Name");
+    private JButton clearButton = new JButton("Clear Item");
+
+
+
+    public CartItem(String selectedItem) {
+        
+        container.setPreferredSize(new Dimension(0,50));
+        itemName.setText(selectedItem);
+        container.add(itemName);
+        container.add(clearButton);
+    }
+
+
+}
